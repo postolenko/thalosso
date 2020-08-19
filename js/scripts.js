ba153f408969550c5debeb21ea8e500c5b1e6432
postolenko <denis.postolenko@gmail.com>
2020-08-19 21:15:44 +0300

    Thalosso

--- js/scripts.js
+++ js/scripts.js
@@ -14,18 +14,6 @@ function getHeaderParams() {
   }
 }
 
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-
-$(window).load(function() {
-
-
-
-});
 
 $(window).resize(function() {
 
